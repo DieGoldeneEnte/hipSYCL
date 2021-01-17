ca0b81b5492736c560b1b120bb3d675c75cce224
Holger Wünsche <holger.o.wuensche@t-online.de>
2021-01-17 10:01:39 +0100

    use aligned simd for reduction

--- include/hipSYCL/sycl/libkernel/host/group_functions.hpp
+++ include/hipSYCL/sycl/libkernel/host/group_functions.hpp
@@ -203,10 +203,20 @@ T leader_reduce(Group g, T *first, T *last, plus<T> binary_op) {
 
   if (g.leader()) {
     result = *(first++);
-    while (first + inc < last) {
-      v_type x = xsimd::load_unaligned(first);
-      result += xsimd::hadd(x);
-      first += inc;
+
+    void* aligned_ptr = first;
+    size_t size = last-first;
+    std::align(alignof(v_type), 1, aligned_ptr, size);
+
+    while (first < aligned_ptr) {
+      result += *(first++);
+    }
+    if (first == aligned_ptr) {
+      while (first + inc < last) {
+        v_type x = xsimd::load_aligned(first);
+        result += xsimd::hadd(x);
+        first += inc;
+      }
     }
     while (first < last) {
       result += *(first++);
